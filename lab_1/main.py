cf0c12a632382e93b65f29581580e94037075a74
AIGUL2 <ajgul.salim@yandex.ru>
2019-10-02 13:29:00 +0300

    добавлены основные функции

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,22 +3,30 @@ Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
 
+def getWordsFrequence(line):
+    dict = {} #словарь
+    stopWords=['lol','kek','cheburek','of']
+    word = ''
+    for i in range(len(line)):
+        letter = line[i].lower()
+        if str.isalpha(letter):
+            word = word+letter
+        elif len(word) != 0:
+            if word in dict:
+                dict[word]+=1
+            elif word not in stopWords:
+                dict[word]=1
+            word = ''
 
-def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
+    sortedByFrequence = sorted(dict,key=dict.get,reverse=True)
+    for i in sortedByFrequence:
+        print (i,dict[i],sep=':')
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    passss
-return filter_stop_words()
+    print('the most frequent word is: ',sortedByFrequence[0])
+    return dict
 
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular wordss
-    """
-    pass
+
+line =  'Mary was quick to realize that she had won the prize that was a desired thing that everyone wanted'
+getWordsFrequence(line)
+
+

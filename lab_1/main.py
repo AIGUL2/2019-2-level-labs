38303e322a74018e95ad4e232a72725cece8f1f4
AIGUL2 <ajgul.salim@yandex.ru>
2019-10-05 01:18:57 +0300

    with out 4 exercise

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,5 +1,5 @@
 def calculate_frequences(str):
-    dict = {} #словарь
+    dict = {} 
     word = ''
     for i in range(len(str)):
         letter = str[i].lower()
@@ -25,17 +25,6 @@ def get_top_n(frequencies, top_n):
     top_n = top_n if len(sortedFrequencies)>top_n else len(sortedFrequencies)
     return tuple(sortedFrequencies[i] for i in range(top_n))
 
-def read_from_file(path_to_file, lines_limit):
-    with open(path_to_file,"r") as myfile:
-        str = ''
-        for i in range (lines_limit):
-            str = str + myfile.readline()
-    return str
-
-def write_to_file(path_to_file, content):
-    with open(path_to_file,"w") as myfile:
-            myfile.writelines(map(lambda word: word+'\n',content))
-    return True
 
 string = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael."
 stopWords = ('lol','kek','cheburek','meet')
@@ -43,5 +32,3 @@ stopWords = ('lol','kek','cheburek','meet')
 dict = calculate_frequences(string)
 dictFiltred = filter_stop_words(dict,stopWords)
 topwords = (get_top_n(dict,5))
-fromFile = get_top_n(calculate_frequences(read_from_file("data.txt",5)),3)
-write_to_file("report.txt",fromFile)

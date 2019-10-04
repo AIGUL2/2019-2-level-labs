229fe4c74f9db0fb3f277ec4c449bcd4a9a3b225
AIGUL2 <ajgul.salim@yandex.ru>
2019-10-05 00:59:27 +0300

    lil bit fail

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,6 +2,48 @@
 Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
+def calculate_frequences(text):
+    dict_f = {}
+    word = ''
+    text = text.lower()
+    for i in text:
+        if i.isalpha() or i == ' ':
+              word += i
+    word_new = word.split()
+    for i in word_new:
+        if i in dict_f:
+            word_counter = word_new.count(i)
+            dict_f[i] = word_counter
+    return dict_f
+
+
+def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
+
+    """    Removes all stop words from the given frequencies dictionary
+
+    """
+    spisok = {}
+    if type(frequencies)== dict and type(stop_words)== tuple:
+    for k, i in frequencies.items():
+        if k not in stop_words:
+            spisok[k] = i
+    return spisok
+
+def get_top_n(frequencies: dict, top_n: int) -> tuple:
+    """
+    Takes first N popular wordss
+    """
+    pass
+"""
+Labour work #1
+Count frequencies dictionary by the given arbitrary text
+"""
+
+
+
+text_=  '''Mary was quick to realize that she had won the prize that was a desired thing that everyone wanted'''
+stop_words = ('i', 'am', 'a', 'human')
+top_n = 2
 
 def getWordsFrequence(line):
     dict = {} #словарь
@@ -25,10 +67,5 @@ def getWordsFrequence(line):
     print('the most frequent word is: ',sortedByFrequence[0])
     return dict
 
-
-line =  'Mary was quick to realize that she had won the prize that was a desired thing that everyone wanted'
+line = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael."
 getWordsFrequence(line)
-
-
-"""проверка github"""
-"""иии"""

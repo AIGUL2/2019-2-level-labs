d087f8b8413503f85e049bfb9cc8424ae82981e3
AIGUL2 <ajgul.salim@yandex.ru>
2019-10-06 20:53:25 +0300

    changed

--- lab_1/main.py
+++ lab_1/main.py
@@ -12,48 +12,28 @@ def calculate_frequences(text):
             dict_f[i] = word_counter
     return dict_f
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict::
+def filter_stop_words(frequencies, stop_words):
     spisok = {}
     if type(frequencies)== dict and type(stop_words)== tuple:
-    for k,i in frequencies.items():
-        if k not in stop_words:
-            spisok[k] = i
-    return spisok
+        for k, i in frequencies.items():
+            if k not in stop_words:
+                spisok[k] = i
+        return spisok
+
 
 def get_top_n(frequencies, top_n):
-    sortedFrequencies = sorted(frequencies, key=frequencies.get, reverse=True)
-    top_n = top_n if len(sortedFrequencies)>top_n else len(sortedFrequencies)
-    return tuple(sortedFrequencies[i] for i in range(top_n))
+    sortedFrequencies = sorted(frequencies, key=for_sort, reverse=True)
+    ad_element = []
+    for i in range(top_n):
+        word = sortedFrequencies[i]
+        ad_element.append(word)
+    return tuple(ad_element)
+
+def for_sort(n):
+    return n[1]
+
 
 
 text = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael."
 stop_words = ('lol','kek','cheburek','meet')
-topwords = (get_top_n(dict,5))
-
-
-
-  ''' 0.5 done work of 4 exercise'''
-def getWordsFrequence(line):
-    dict = {} #словарь
-    stopWords=['lol','kek','cheburek','of']
-    word = ''
-    for i in range(len(line)):
-        letter = line[i].lower()
-        if str.isalpha(letter):
-            word = word+letter
-        elif len(word) != 0:
-            if word in dict:
-                dict[word]+=1
-            elif word not in stopWords:
-                dict[word]=1
-            word = ''
-
-    sortedByFrequence = sorted(dict,key=dict.get,reverse=True)
-    for i in sortedByFrequence:
-        print (i,dict[i],sep=':')
-
-    print('the most frequent word is: ',sortedByFrequence[0])
-    return dict
-
 line = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael."
-getWordsFrequence(line)

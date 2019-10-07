a7441e92f12800b967ff7091385bcac15caf53b7
AIGUL2 <ajgul.salim@yandex.ru>
2019-10-07 14:14:27 +0300

    add type

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,37 +1,44 @@
-def calculate_frequences(str):
-    dict = {} #словарь
-    word = '' #переменная для очередного слова
-    for i in range(len(str)): #проходим по всем буквам/символам
-        letter = str[i].lower() #переводим в нижний регистр
-        if letter.isalpha(): #если это буква, а не символ или че то другое
-            word = word+letter #добавляем букву в слово
-        elif len(word) != 0: #если же  очередой символ не является буквой, значит это конец слова
-            if word in dict:
-                dict[word]+=1 #то увеличиваем "встречаемость"
-            else: # иначе добавляем слово в словарь и ставим встречаемость =1
-                dict[word]=1
-            word = '' # обнуляем переменную под очередное слово
+def calculate_frequencies(text: str) -> dict:
+    dict = {}
+    word = ''
+    if type(text) == str:
+        for i in range(len(text)):
+            letter = text[i].lower()
+            if letter.isalpha():
+                word = word + letter
+            elif len(word) != 0:  # если же  очередой символ не является буквой, значит это конец слова
+                if word in dict:
+                    dict[word] += 1
+                else:  # иначе добавляем слово в словарь и ставим встречаемость =1
+                    dict[word] = 1
+                word = ''
     return dict
 
 
 
-def filter_stop_words(frequencies,stop_words):
-    dictcopy = frequencies.copy()
-    for word in stop_words:
-        if word in dictcopy:
-            del dictcopy[word]
-    return dictcopy
+def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
+    if stop_words is None or frequencies is None:
+        return {}
+    for key in stop_words:
+        if key in frequencies:
+            frequencies.pop(key)
+
+    result_dict = {}
+    for key, value in frequencies.items():
+        if type(key) is str:
+            result_dict[key] = value
+
+    return result_dict
 
 
 def get_top_n(frequencies, top_n):
     sortedFrequencies = sorted(frequencies, key=frequencies.get, reverse=True)
-    top_n = top_n if len(sortedFrequencies)>top_n else len(sortedFrequencies) # тут проверка, меньше ли число слов, которое мы хотим получить, чем вообще длина словаря, если да, то ничего не делаем, иначе - возвращаем весь словарь
-    return tuple(sortedFrequencies[i] for i in range(top_n)) # создаем tuple из словаря (внутри находится "генератор", прочитай в инетике чо ето такое)
-
+    top_n = top_n if len(sortedFrequencies)>top_n else len(sortedFrequencies) # тут проверка, меньше ли число слов, чем  длина словаря
+    return tuple(sortedFrequencies[i] for i in range(top_n))
 
 string = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael."
 stopWords = ('lol','kek','cheburek','meet')
-dict = calculate_frequences(string)
+dict = calculate_frequencies(string)
 dictFiltred = filter_stop_words(dict,stopWords)
 topwords = (get_top_n(dict,5))
 

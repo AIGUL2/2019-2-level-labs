b5a61290dd129bc2289bdbc5cb76bf2f083fb8e0
AIGUL2 <ajgul.salim@yandex.ru>
2019-10-08 10:28:22 +0300

    deleted items below in functions

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,32 +1,35 @@
 def calculate_frequencies(text: str) -> dict:
-    dict = {}
-    word = ''
-    if type(text) == str:
-        for i in range(len(text)):
-            letter = text[i].lower()
-            if letter.isalpha():
-                word = word + letter
-            elif len(word) != 0:  # если же  очередой символ не является буквой, значит это конец слова
-                if word in dict:
-                    dict[word] += 1
-                else:  # иначе добавляем слово в словарь и ставим встречаемость =1
-                    dict[word] = 1
-                word = ''
-    return dict
+    dict_f = {}
+    if text == "" or text is None:
+        return dict_f
+    text = str(text)
+    text_lower = text.lower()
+    for a in text_lower:
+        if not a.isalpha():
+            text_lower = text_lower.replace(a, ' ')
+    split = text_lower.split()
+    for word in split:
+        word = word.lower()
+        if word  in dict_f():
+            value = dict_f[word]
+            dict_f[word] = value + 1
+        else:
+            dict_f[word] = 1
+    return dict_f
+
 
 
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     if stop_words is None or frequencies is None:
         return {}
-    for key in stop_words:
-        if key in frequencies:
-            frequencies.pop(key)
-
+    for word in stop_words:
+        if word in frequencies:
+            frequencies.pop(word)
     result_dict = {}
-    for key, value in frequencies.items():
-        if type(key) is str:
-            result_dict[key] = value
+    for word, value in frequencies.items():
+        if type(word) is str:
+            result_dict[word] = value
 
     return result_dict
 
@@ -35,10 +38,4 @@ def get_top_n(frequencies, top_n):
     sortedFrequencies = sorted(frequencies, key=frequencies.get, reverse=True)
     top_n = top_n if len(sortedFrequencies)>top_n else len(sortedFrequencies) # тут проверка, меньше ли число слов, чем  длина словаря
     return tuple(sortedFrequencies[i] for i in range(top_n))
-
-string = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael."
-stopWords = ('lol','kek','cheburek','meet')
-dict = calculate_frequencies(string)
-dictFiltred = filter_stop_words(dict,stopWords)
-topwords = (get_top_n(dict,5))
 

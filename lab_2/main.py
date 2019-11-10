d16fbe493c335e4b42c77ed6525069439c131555
AIGUL2 <ajgul.salim@yandex.ru>
2019-11-11 01:33:17 +0300

    the 2 func

--- lab_2/main.py
+++ lab_2/main.py
@@ -12,10 +12,27 @@ def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
         for j in range(num_rows):
             matrix.append(row.copy())
 
+    return matrix
+
     pass
 
 
 def initialize_edit_matrix(edit_matrix: tuple, add_weight: int, remove_weight: int) -> list:
+    edit_matrix = list(edit_matrix)
+
+    if len(edit_matrix) == 0 or len(edit_matrix[0]) == 0 or not add_weight or not remove_weight:
+        return edit_matrix
+
+    for i in range(1, len(edit_matrix)):
+        edit_matrix[i][0] = edit_matrix[i - 1][0] + remove_weight
+
+    for j in range(1, len(edit_matrix[0])):
+        edit_matrix[0][j] = edit_matrix[0][j - 1] + add_weight
+    print(edit_matrix)
+
+    return edit_matrix
+
+    initialize_edit_matrix(([0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]), 1, 1)
     pass
 
 

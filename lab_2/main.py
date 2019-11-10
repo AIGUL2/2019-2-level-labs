16ebaf8d740441d06ab9d0afcbe62f5f09213878
AIGUL2 <ajgul.salim@yandex.ru>
2019-11-11 01:25:44 +0300

    the first function

--- lab_2/main.py
+++ lab_2/main.py
@@ -4,6 +4,14 @@ Labour work #2. Levenshtein distance.
 
 
 def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
+    def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
+        matrix = []
+        row = []
+        for i in range(num_cols):
+            row.append(0)
+        for j in range(num_rows):
+            matrix.append(row.copy())
+
     pass
 
 
